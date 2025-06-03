5adf89551b9f60f4fd09a2ae4e5ec4dbbda17b30
PhucLHSE <phuclhse173083@fpt.edu.vn>
2025-06-03 21:35:21 +0700

    feat(backend): initialize multi-project architecture (API, Service, Repository, Common)

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Repositories/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using DakLakCoffeeSupplyChain.Repositories.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Repositories
+{
+    public class UnitOfWork
+    {
+        private DakLakCoffee_SCMContext context;
+
+        public UnitOfWork()
+        {
+            context ??= new DakLakCoffee_SCMContext();
+        }
+    }
+}
---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Services/Base/ServiceResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Services.Base
+{
+    public class ServiceResult : IServiceResult
+    {
+        public int Status { get; set; }
+        public string? Message { get; set; }
+        public object? Data { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<string>? Errors { get; set; }
+
+        public ServiceResult()
+        {
+            Status = -1;
+            Message = "Action fail";
+        }
+
+        public ServiceResult(int status, string message)
+        {
+            Status = status;
+            Message = message;
+        }
+
+        public ServiceResult(int status, string message, object data)
+        {
+            Status = status;
+            Message = message;
+            Data = data;
+        }
+
+        public ServiceResult(int status, string message, List<string> errors)
+        {
+            Status = status;
+            Message = message;
+            Errors = errors;
+        }
+    }
+}
