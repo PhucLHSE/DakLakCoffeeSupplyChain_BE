bf57237356a744d1b643e5e2d591fd671882075a
PhucLHSE <phuclhse173083@fpt.edu.vn>
2025-06-09 20:45:29 +0700

    feat(role): implement GetAll Role API with DTO mapping and service integration

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Common/Enum/RoleEnums/RoleStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Common.Enum.RoleEnums
+{
+    public enum RoleStatus
+    {
+        Active = 1,
+        Inactive = 0
+    }
+}
--- Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Repositories/IRepositories/ISystemConfigurationRepository.cs
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Repositories/IRepositories/ISystemConfigurationRepository.cs
@@ -1,4 +1,5 @@
-﻿using DakLakCoffeeSupplyChain.Repositories.Models;
+﻿using DakLakCoffeeSupplyChain.Repositories.Base;
+using DakLakCoffeeSupplyChain.Repositories.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace DakLakCoffeeSupplyChain.Repositories.IRepositories
 {
-    public interface ISystemConfigurationRepository
+    public interface ISystemConfigurationRepository : IGenericRepository<SystemConfiguration>
     {
         Task<SystemConfiguration?> GetActiveByNameAsync(string name);
     }
