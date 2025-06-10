702b345521f81dbf1c3e467578563333b6f0ef4e
Lehoangthienvu <Vulhtse160593@fpt.edu.vn>
2025-06-10 14:57:35 +0700

    feat(processingMethod): implement DeleteById Processing Method API with DTO mapping and service integration

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Common/DTOs/Flow3DTOs/ProcessingMethod/ProcessingMethodDeleteDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Common.DTOs.Flow3DTOs.ProcessingMethod
+{
+    public class ProcessingMethodDeleteDto
+    {
+        public int MethodId { get; set; }
+    }
+}
