3bbd9a6bf7f99ba451e434d1fb990ff422797fc0
Namsuper12345 <namptse150442@fpt.edu.vn>
2025-06-18 12:36:16 +0700

    feat(warehouse): implement update functionality with validation and mapping

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Common/DTOs/WarehouseDTOs/WarehouseUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Common.DTOs.WarehouseDTOs
+{
+    public class WarehouseUpdateDto
+    {
+        public Guid WarehouseId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public double? Capacity { get; set; }
+        public Guid ManagerId { get; set; }
+    }
+}
---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Services/Mappers/WarehouseMapper.cs
@@ -0,0 +1,22 @@
+﻿using DakLakCoffeeSupplyChain.Common.DTOs.WarehouseDTOs;
+using DakLakCoffeeSupplyChain.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Services.Mappers
+{
+    public static class WarehouseMapper
+    {
+        public static void MapToEntity(this WarehouseUpdateDto dto, Warehouse entity)
+        {
+            entity.Name = dto.Name;
+            entity.Location = dto.Location;
+            entity.Capacity = dto.Capacity;
+            entity.ManagerId = dto.ManagerId;
+            entity.UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
