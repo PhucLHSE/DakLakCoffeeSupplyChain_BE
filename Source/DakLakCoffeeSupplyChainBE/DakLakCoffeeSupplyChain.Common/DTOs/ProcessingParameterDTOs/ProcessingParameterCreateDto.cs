476b0106b1a31484ffd9ec713055c1b3ec6b299b
Lehoangthienvu <Vulhtse160593@fpt.edu.vn>
2025-06-24 13:19:28 +0700

    feat(processing-parameter): implement Create API with duplicate name validation

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Common/DTOs/ProcessingParameterDTOs/ProcessingParameterCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Common.DTOs.ProcessingParameterDTOs
+{
+    public class ProcessingParameterCreateDto
+    {
+        public Guid ProgressId { get; set; }
+        public string ParameterName { get; set; } = default!;
+        public string ParameterValue { get; set; } = default!;
+        public string Unit { get; set; } = default!;
+        public DateTime? RecordedAt { get; set; }
+    }
+}
