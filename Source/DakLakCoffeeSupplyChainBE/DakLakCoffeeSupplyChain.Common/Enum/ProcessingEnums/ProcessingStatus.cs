44a1365ef7c429bd1d2e5fdafdcd2a7b6ea89eaf
Lehoangthienvu <Vulhtse160593@fpt.edu.vn>
2025-07-18 13:20:02 +0700

    fix(processing-batch): switch Status field to enum type

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Common/Enum/ProcessingEnums/ProcessingStatus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Common.Enum.ProcessingEnums
+{
+    public enum ProcessingStatus
+    {
+        [Display(Name = "Chưa bắt đầu")]
+        NotStarted = 0,
+
+        [Display(Name = "Đang xử lý")]
+        InProgress = 1,
+
+        [Display(Name = "Hoàn thành")]
+        Completed = 2,
+
+        [Display(Name = "Đã hủy")]
+        Cancelled = 3
+    }
+}
