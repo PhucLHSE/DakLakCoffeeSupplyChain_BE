0dc24ba75e453be5bd34d2c0c5201bfe0b7d7831
Minh <minhnnse161013@fpt.edu.vn>
2025-08-13 11:30:53 +0700

    Update CropSeasonDetailController and service interfaces for crop season & progress handling

--- Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Services/IServices/ICropSeasonService.cs
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Services/IServices/ICropSeasonService.cs
@@ -4,7 +4,7 @@ using DakLakCoffeeSupplyChain.Services.Base;
 public interface ICropSeasonService
 {
     Task<IServiceResult> GetAllByUserId(Guid userId, bool isAdmin, bool isManager);
-    Task<IServiceResult> GetById(Guid cropSeasonId, Guid userId, bool isAdmin = false);
+    Task<IServiceResult> GetById(Guid cropSeasonId, Guid userId, bool isAdmin = false, bool isManager = false);
     Task<IServiceResult> Create(CropSeasonCreateDto dto, Guid userId);
     Task<IServiceResult> Update(CropSeasonUpdateDto dto, Guid userId, bool isAdmin = false);
     Task<IServiceResult> DeleteById(Guid cropSeasonId, Guid userId, bool isAdmin);
