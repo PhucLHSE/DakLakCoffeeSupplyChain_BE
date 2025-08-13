5d824a4ef4c8e8d2a737b2e93099d0a9b732c896
Lehoangthienvu <vulhtse160593@fpt.edu.vn>
2025-08-13 19:02:49 +0700

    reslove conflic

--- Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Services/IServices/INotificationService.cs
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Services/IServices/INotificationService.cs
@@ -15,5 +15,14 @@ namespace DakLakCoffeeSupplyChain.Services.IServices
         Task<SystemNotification> NotifyInboundRequestApprovedAsync(Guid requestId, Guid farmerId);
         Task<SystemNotification> NotifyOutboundRequestCreatedAsync(Guid requestId, Guid staffId);
         Task<SystemNotification> NotifyEvaluationFailedAsync(Guid batchId, Guid farmerId, string evaluationComments);
+        
+        Task<SystemNotification> NotifyFarmerReportCreatedAsync(Guid reportId, Guid farmerId, string reportTitle);
+        Task<SystemNotification> NotifyExpertAdviceCreatedAsync(Guid reportId, Guid expertId, string expertName, string adviceText);
+
+        Task<IServiceResult> GetUserNotificationsAsync(Guid userId, int page, int pageSize);
+        Task<IServiceResult> GetUnreadCountAsync(Guid userId);
+        Task<IServiceResult> MarkAsReadAsync(Guid notificationId, Guid userId);
+        Task<IServiceResult> MarkAllAsReadAsync(Guid userId);
+        Task<IServiceResult> GetNotificationByIdAsync(Guid notificationId, Guid userId);
     }
 }
