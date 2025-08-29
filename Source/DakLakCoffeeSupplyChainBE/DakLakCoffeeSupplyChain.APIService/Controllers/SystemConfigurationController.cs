51207204a749c5035e5523b8150cfb8eb8d246cc
xuangand854 <dangphxse161782@fpt.edu.vn>
2025-08-29 09:09:03 +0700

    feat(SystemConfiguration) Implement GET ALL and GET BY NAME system configuration API

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.APIService/Controllers/SystemConfigurationController.cs
@@ -0,0 +1,62 @@
+﻿using DakLakCoffeeSupplyChain.Common;
+using DakLakCoffeeSupplyChain.Common.Helpers;
+using DakLakCoffeeSupplyChain.Services.IServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+
+namespace DakLakCoffeeSupplyChain.APIService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemConfigurationController (ISystemConfigurationService service) : ControllerBase
+    {
+        private readonly ISystemConfigurationService _service = service;
+
+        [HttpGet]
+        [EnableQuery]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            Guid userId;
+
+            try
+            {
+                // Lấy userId từ token qua ClaimsHelper
+                userId = User.GetUserId();
+            }
+            catch
+            {
+                return Unauthorized("Không xác định được userId từ token.");
+            }
+
+            var result = await _service
+                .GetAll(userId);
+
+            if (result.Status == Const.SUCCESS_READ_CODE)
+                return Ok(result.Data);              // Trả đúng dữ liệu
+
+            if (result.Status == Const.WARNING_NO_DATA_CODE)
+                return NotFound(result.Message);     // Trả 404 + message
+
+            return StatusCode(500, result.Message);  // Trả 500 + message
+        }
+
+        [HttpGet("{configName}")]
+        [EnableQuery]
+        public async Task<IActionResult> GetAllAsync(string configName)
+        {
+
+            var result = await _service
+                .GetByName(configName);
+
+            if (result.Status == Const.SUCCESS_READ_CODE)
+                return Ok(result.Data);              // Trả đúng dữ liệu
+
+            if (result.Status == Const.WARNING_NO_DATA_CODE)
+                return NotFound(result.Message);     // Trả 404 + message
+
+            return StatusCode(500, result.Message);  // Trả 500 + message
+        }
+    }
+}
