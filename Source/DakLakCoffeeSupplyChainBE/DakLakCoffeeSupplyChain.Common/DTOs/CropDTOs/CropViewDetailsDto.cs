e8b463614135613ac1b267dc96d2d0b8c7cb2789
PhucLHSE <phuclhse173083@fpt.edu.vn>
2025-09-25 11:31:35 +0700

    feat(crop): implement GetById API for Farmer (Controller, Service, DTO, Mapper)

---
+++ Source/DakLakCoffeeSupplyChainBE/DakLakCoffeeSupplyChain.Common/DTOs/CropDTOs/CropViewDetailsDto.cs
@@ -0,0 +1,38 @@
+﻿using DakLakCoffeeSupplyChain.Common.Enum.CropEnums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DakLakCoffeeSupplyChain.Common.DTOs.CropDTOs
+{
+    public class CropViewDetailsDto
+    {
+        public Guid CropId { get; set; }
+
+        public string CropCode { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string FarmName { get; set; } = string.Empty;
+
+        public decimal? CropArea { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public CropStatus Status { get; set; } = CropStatus.Other;
+
+        public DateTime? CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public Guid? CreatedBy { get; set; }
+
+        public Guid? UpdatedBy { get; set; }
+
+        public string CreatedByName { get; set; } = "N/A";
+
+        public string UpdatedByName { get; set; } = "N/A";
+    }
+}
